48b4d3d762f5bb21c1f4465205586dd4c5d09cd5
tsclausing <tsclausing@gmail.com>
2013-10-24 22:51:38 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# 4clojure Problem Solutions
+
+Just a humble learner, here.
+
+http://www.4clojure.com/user/tsclausing
---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject com.4clojure "0.1.0-SNAPSHOT"
+  :description "4clojure.com problem solutions"
+  :url "http://www.4clojure.com/user/tsclausing"
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :main com.4clojure
+  :profiles {:uberjar {:aot :all}})
