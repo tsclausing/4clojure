bcbd3f1a18d92a471542222b821a131715b66aa4
tsclausing <tsclausing@gmail.com>
2013-11-20 20:43:14 -0600

    Criterium dependency for performance benchmarking

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject com.4clojure "0.1.0-SNAPSHOT"
   :description "4clojure.com problem solutions"
   :url "http://www.4clojure.com/user/tsclausing"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [criterium "0.4.2"]]
   :main com.4clojure
   :profiles {:uberjar {:aot :all}})
